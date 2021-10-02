ed0cff5852ea2d04bcea3874d50a06602b4f31f4
Stephane WAFFO <guywaffo@gmail.com>
2021-10-02 16:24:21 +0200

    "Mise à jour des scripts gcp - utilisation de docker"

--- scripts-gcp/tensorboard.sh
+++ scripts-gcp/tensorboard.sh
@@ -1,1 +1,1 @@
-sudo  docker-compose exec app tensorboard  --host "34.81.16.135" --port 6006 --logdir ../app/logs
+sudo  docker-compose exec app tensorboard  --host 0.0.0.0 --port 6006 --logdir ../app/logs
