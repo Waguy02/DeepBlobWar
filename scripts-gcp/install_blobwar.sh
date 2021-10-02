db295c67ea6b0c72f6314f957cb79f5c79c4f7f6
Stephane WAFFO <guywaffo@gmail.com>
2021-10-02 22:16:47 +0200

    "Add tictactoe scripts"

--- scripts-gcp/install_blobwar.sh
+++ scripts-gcp/install_blobwar.sh
@@ -1,3 +1,2 @@
 sudo chmod -R 777 ../*
-sudo docker-compose up -d
 sudo docker-compose exec app pip3 install -e ./environments/blobwar
--- scripts-gcp/install_tictac.sh
+++ scripts-gcp/install_tictac.sh
@@ -1,2 +1,1 @@
-sudo docker-compose up -d
 sudo docker-compose exec app pip3 install -e ./environments/sushigo
