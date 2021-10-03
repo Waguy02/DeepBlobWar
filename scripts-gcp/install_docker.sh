1314a915ae2a90da110f744a7b9997984509b861
Stephane WAFFO <guywaffo@gmail.com>
2021-10-03 21:37:33 +0200

    "SIZE 8 READY"

--- scripts-gcp/install_docker.sh
+++ scripts-gcp/install_docker.sh
@@ -1,5 +1,7 @@
 sudo apt update
 sudo apt upgrade
+sudo SNAP install docker
 sudo apt install curl
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose up -d
