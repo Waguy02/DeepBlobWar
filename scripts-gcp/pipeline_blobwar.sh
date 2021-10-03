a0157318ad60672404ccd83cb2026ed05a6e371c
Stephane WAFFO <guywaffo@gmail.com>
2021-10-03 19:08:54 +0200

    "Blowbar train pipeline"

--- scripts-gcp/pipeline_blobwar.sh
+++ scripts-gcp/pipeline_blobwar.sh
@@ -1,5 +1,5 @@
 sudo chmod -R 777 ../*
 sudo docker-compose up -d
-docker-compose exec app tensorboard --logdir ./logs &
+sudo /bin/bash tensorboard.sh &
 sudo docker-compose exec app pip3 install -e ./environments/blobwar
 docker-compose  exec app mpirun -np 8 python3 train.py -r  -e blobwar -o best -oe 3
