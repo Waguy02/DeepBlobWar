54131d681e1d9fe080c078b982d86c8de6546260
Stephane WAFFO <guywaffo@gmail.com>
2021-10-04 00:30:57 +0200

    "update number of epochs to 5"

--- scripts-gcp/pipeline_blobwar.sh
+++ scripts-gcp/pipeline_blobwar.sh
@@ -1,4 +1,4 @@
 sudo chmod -R 777 ../*
 sudo docker-compose up -d
 sudo docker-compose exec app pip3 install -e ./environments/blobwar
-docker-compose  exec app mpirun -np 8 python3 train.py  -e blobwar -o best -oe 5
+docker-compose  exec app mpirun -np 6 python3 train.py -r  -e blobwar -o best -oe 5
