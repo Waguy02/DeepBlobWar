e707e292b7e39dae14ff3c06c87ad749d5235d3d
Stephane WAFFO <guywaffo@gmail.com>
2021-10-05 08:46:01 +0200

    "Update  scripts download  files"

--- scripts/download_model_size_8.bat
+++ scripts/download_model_size_8.bat
@@ -1,2 +1,2 @@
-scp -r waguy@34.130.200.138:/home/waguyfake6/DeepBlobWar/app/zoo/blobwar  "B:\Projets\BlobWar\DeepBlobWar\app\zoo\blobwar\model_size_8"
-scp -r waguy@34.130.200.138:/home/waguyfake6/DeepBlobWar/app/logs  "B:\Projets\BlobWar\DeepBlobWar\app\zoo\blobwar\model_size_8"
+scp waguy@34.130.200.138:/home/waguyfake6/DeepBlobWar/app/zoo/blobwar/best_model.zip   "B:\Projets\BlobWar\DeepBlobWar\app\zoo\blobwar\model_size_8\best_model8.zip"
+scp waguy@34.130.200.138:/home/waguyfake6/DeepBlobWar/app/logs/log.txt   "B:\Projets\BlobWar\DeepBlobWar\app\zoo\blobwar\model_size_8\logs.txt"
