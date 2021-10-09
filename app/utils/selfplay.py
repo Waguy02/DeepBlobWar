238f1ddc39391128ebd58793c9c622d48391a8df
Stephane WAFFO <guywaffo@gmail.com>
2021-10-09 19:07:31 +0200

    "Training with SIZE 6"

--- app/utils/selfplay.py
+++ app/utils/selfplay.py
@@ -86,7 +86,7 @@ def selfplay_wrapper(env):
                 action = self.current_agent.choose_action(self, choose_best_action = False, mask_invalid_actions = False)
 
                 if self.opponent_type == 'rules':
-                    assert(self.env.core.check_move(self.env.decode_action(action)),"Adversary rules based cannot play bad moves")
+                    assert(self.core.check_move(self.decode_action(action)),"Adversary rules based cannot play bad moves")
 
                 observation, rewards, done, _ = super(SelfPlayEnvBlobwar, self).step(action)
                 # formatted_action = super(SelfPlayEnvBlobwar, self).decode_action(action)
