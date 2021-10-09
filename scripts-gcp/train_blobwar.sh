e9152d1b2fe2f4bb7a7afd05148792ca020b8b75
Stephane WAFFO <guywaffo@gmail.com>
2021-10-09 14:55:28 +0200

    "Highly penalize bad moves"

--- scripts-gcp/train_blobwar.sh
+++ scripts-gcp/train_blobwar.sh
@@ -1,2 +1,2 @@
 sudo docker-compose up -d
-sudo docker-compose  exec app python3 train.py -r  -e blobwar -o rules -oe 4
+sudo docker-compose  exec app mpirun -np 7 python3 train.py -r  -e blobwar -o rules -oe 4
